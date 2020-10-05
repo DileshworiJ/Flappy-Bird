37b7d14dbdd68ab0035cab736f989d65e92baf06
Dileshwori <dileshowri.joshi@sagarmatha.edu.np>
2020-10-05 12:08:31 +0545

    placing image in the surface

--- flappy.py
+++ flappy.py
@@ -3,10 +3,18 @@ import pygame, sys
 pygame.init()  #to initialize pygame library
 
 screen = pygame.display.set_mode((288,512))  #method called display, set_mode takes width and height
+clock = pygame.time.Clock()
+
+bg_surface = pygame.image.load('assets/background-day.png').convert()           #adding background image #convert into the type that pygame feels easier, hepls ith consistency
+#scale image to screen dimension
+#bg_surface = pygame.transform.scale2x(bg_surface)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()              #quit method
+            sys.exit()             #quit method
+    screen.blit(bg_surface,(0,0))    #to display the image in the display screen
+
     pygame.display.update()
+    clock.tick(120)  # framepersecond
