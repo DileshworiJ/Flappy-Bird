ab9fbf2b7112e24a9809e6cba5dc4289589672e8
Dileshwori <dileshowri.joshi@sagarmatha.edu.np>
2020-10-06 13:47:56 +0545

    creating pipes

--- flappy.py
+++ flappy.py
@@ -1,8 +1,21 @@
 import pygame, sys
+
 def draw_base():
     screen.blit(base_surface, (base_x_pos,450))
     screen.blit(base_surface, (base_x_pos +288,450))
 
+def create_pipe():
+    new_pipe =pipe_surface.get_rect(midtop= (350,256))
+    return new_pipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 2.5
+    return pipes 
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        screen.blit(pipe_surface,pipe)
 
 pygame.init()  #to initialize pygame library
 
@@ -19,6 +32,10 @@ base_x_pos = 0
 bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
 bird_rect = bird_surface.get_rect(center = (50,200))
 
+pipe_surface = pygame.image.load('assets/pipe-green.png').convert()
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT                #TRIGGERED BY A TIMER USEREVENT
+pygame.time.set_timer(SPAWNPIPE,1200)           #time in millisecond, 1.2sec
 #scale image to screen dimension
 #bg_surface = pygame.transform.scale2x(bg_surface)
 
@@ -31,11 +48,19 @@ while True:
             if event.key ==pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 6
+        if event.type == SPAWNPIPE:         #checking for a condition when == is used
+            pipe_list.append(create_pipe())
+            print(pipe_list)
+
     screen.blit(bg_surface,(0,0))
     
     bird_movement +=gravity
     bird_rect.centery += int(bird_movement)
     screen.blit(bird_surface,bird_rect)
+
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
     # screen.blit(bg_surface,(0,0))    #to display the image in the display screen
     base_x_pos -=1
     draw_base()
